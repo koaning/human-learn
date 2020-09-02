02b13cdaf4f27ab78947db8b0816b7bc758de5d5
vincent d warmerdam <vincentwarmerdam@gmail.com>
2020-09-02 20:01:28 +0200

    function-regr (#9)

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,17 @@ np_types = (np.int32, np.float32, np.float64)
 @pytest.fixture(
     scope="module", params=[_ for _ in it.product(n_vals, k_vals, np_types)]
 )
+def random_xy_dataset_regr(request):
+    n, k, np_type = request.param
+    np.random.seed(42)
+    X = np.random.normal(0, 2, (n, k)).astype(np_type)
+    y = np.random.normal(0, 2, (n,))
+    return X, y
+
+
+@pytest.fixture(
+    scope="module", params=[_ for _ in it.product(n_vals, k_vals, np_types)]
+)
 def random_xy_dataset_clf(request):
     n, k, np_type = request.param
     np.random.seed(42)
---
+++ tests/test_regression/test_functionregressor.py
@@ -0,0 +1,47 @@
+import pytest
+import numpy as np
+
+from sklearn.model_selection import GridSearchCV
+from hulearn.regression.functionregressor import FunctionRegressor
+from hulearn.common import flatten
+
+from tests.conftest import (
+    select_tests,
+    general_checks,
+    regressor_checks,
+    nonmeta_checks,
+)
+
+
+def predict(X):
+    np.random.seed(42)
+    return np.array([1 if r > 0.5 else 0 for r in np.random.normal(0, 1, len(X))])
+
+
+def predict_variant(X):
+    np.random.seed(42)
+    return np.array([1 if r > 0.0 else 0 for r in np.random.normal(0, 1, len(X))])
+
+
+@pytest.mark.parametrize(
+    "test_fn",
+    select_tests(
+        include=flatten([general_checks, regressor_checks, nonmeta_checks]),
+        exclude=[
+            "check_methods_subset_invariance",
+            "check_fit2d_1sample",
+            "check_fit2d_1feature",
+            "check_regressors_train",
+        ],
+    ),
+)
+def test_estimator_checks(test_fn):
+    clf = FunctionRegressor(func=predict)
+    test_fn(FunctionRegressor.__name__ + "_fallback", clf)
+
+
+def test_works_with_gridsearch(random_xy_dataset_regr):
+    X, y = random_xy_dataset_regr
+    clf = FunctionRegressor(func=predict)
+    grid = GridSearchCV(clf, cv=5, param_grid={"func": [predict, predict_variant]})
+    grid.fit(X, y).predict(X)
