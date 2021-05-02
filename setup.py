57ff8c08ab09d74b2bf1b88a9ca0ac358a8a3185
koaning <vincentwarmerdam@gmail.com>
2021-05-02 12:27:02 +0200

    add-py3.8

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
