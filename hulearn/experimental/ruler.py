28c01b7f9f7b0f0fb5b7e5f71351d50572b31986
koaning <vincentwarmerdam@gmail.com>
2021-05-02 12:14:38 +0200

    casewhen

--- hulearn/experimental/ruler.py
+++ hulearn/experimental/ruler.py
@@ -17,7 +17,9 @@ class CaseWhenRuler:
 
     ```python
     from hulearn.datasets import load_titanic
+    from hulearn.experimental import CaseWhenRuler
     from hulearn.classification import FunctionClassifier
+
 
     def make_prediction(dataf, age=15):
         ruler = CaseWhenRuler(default=0)
@@ -57,7 +59,49 @@ class CaseWhenRuler:
         Usage:
 
         ```python
+        from hulearn.classification import FunctionClassifier
+        from hulearn.experimental import CaseWhenRuler
+
+        def make_prediction(dataf, gender_rule=True, child_rule=True, fare_rule=True):
+            ruler = CaseWhenRuler(default=0)
+
+            if gender_rule:
+                ruler.add_rule(when=lambda d: (d['pclass'] < 3.0) & (d['sex'] == "female"),
+                               then=1,
+                               name="gender-rule")
+
+            if child_rule:
+                ruler.add_rule(when=lambda d: (d['pclass'] < 3.0) & (d['age'] <= 15),
+                               then=1,
+                               name="child-rule")
+
+            if fare_rule:
+                ruler.add_rule(when=lambda d: (d['fare'] > 100),
+                               then=1,
+                               name="fare-rule")
+
+            return ruler.transform(dataf)
+
+        clf = FunctionClassifier(make_prediction)
+        ```
+        """
+        results = [self.default for x in range(len(X))]
+        for rule in self.rules:
+            when, then, name = rule
+            for idx, predicate in enumerate(when(X)):
+                if predicate and (results[idx] == self.default):
+                    results[idx] = then
+        return results
+
+    def transform(self, X):
+        """
+        Produces a dataframe that indicates the state of all rules.
+
+        Usage:
+
+        ```python
         from hulearn.preprocessing import PipeTransformer
+        from hulearn.experimental import CaseWhenRuler
 
         def make_prediction(dataf, gender_rule=True, child_rule=True, fare_rule=True):
             ruler = CaseWhenRuler(default=0)
@@ -80,44 +124,6 @@ class CaseWhenRuler:
             return ruler.transform(dataf)
 
         clf = PipeTransformer(make_prediction)
-        ```
-        """
-        results = [self.default for x in range(len(X))]
-        for rule in self.rules:
-            when, then, name = rule
-            for idx, predicate in enumerate(when(X)):
-                if predicate and (results[idx] == self.default):
-                    results[idx] = then
-        return results
-
-    def transform(self, X):
-        """
-        Produces a dataframe that indicates the state of all rules.
-
-        Usage:
-
-        ```python
-        def make_prediction(dataf, gender_rule=True, child_rule=True, fare_rule=True):
-            ruler = CaseWhenRuler(default=0)
-
-            if gender_rule:
-                ruler.add_rule(when=lambda d: (d['pclass'] < 3.0) & (d['sex'] == "female"),
-                               then=1,
-                               name="gender-rule")
-
-            if child_rule:
-                ruler.add_rule(when=lambda d: (d['pclass'] < 3.0) & (d['age'] <= 15),
-                               then=1,
-                               name="child-rule")
-
-            if fare_rule:
-                ruler.add_rule(when=lambda d: (d['fare'] > 100),
-                               then=1,
-                               name="fare-rule")
-
-            return ruler.transform(dataf)
-
-        clf = FunctionClassifier(make_prediction)
         ```
         """
         result = pd.DataFrame()
